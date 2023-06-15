d1d65d547c27f09fd2be9aa90ca04535be45adcc
Anshul Ranjan <ItsAnshulRanjan@gmail.com>
2023-06-15 23:41:47 +0530

    contact form done

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,55 +1,46 @@
-const { Client } = require('@notionhq/client');
-
-const notion = new Client({
-  auth: process.env.NOTION_API_TOKEN,
-});
-
-export default async (req, res) => {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ msg: 'Only POST requests are allowed' });
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return new Response(
+      JSON.stringify({
+        name: 'Only POST requests allowed.',
+      }),
+      {
+        status: 405,
+        headers: {
+          'content-type': 'application/json',
+        },
+      }
+    );
   }
-  try {
-    const { name, email, subject, message } = JSON.parse(req.body);
-    await notion.pages.create({
-      parent: {
-        database_id: process.env.NOTION_DATABASE_ID,
-      },
-      properties: {
-        Name: {
-          title: [
+  try{
+  const { name, email, subject, message } = JSON.parse(req.body);
+    if (!name || !email || !message) {
+        return new Response(
+            JSON.stringify({
+                message: "Missing required fields",
+            }),
+            { status: 400 }
+        );
+    }
+    const discord_webhook_url = "https://discord.com/api/webhooks/1118963806681452605/aJ34FDpT196dUBGK3NsqdRx2VGlsqHcvqN7Q1sBNOZvbJ3eiAK0xMb87pWGoeZNooA2-"
+    await fetch(discord_webhook_url, {
+        method: "post",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
             {
-              text: {
-                content: name,
-              },
-            },
-          ],
-        },
-        Email: {
-          email,
-        },
-        Subject: {
-          rich_text: [
-            {
-              text: {
-                content: subject,
-              },
-            },
-          ],
-        },
-        Message: {
-          rich_text: [
-            {
-              text: {
-                content: message,
-              },
-            },
-          ],
-        },
-      },
-    });
+                "content": `**Name**: ${name}\n**Email**: ${email}\n**__Subject__**:\n${subject}\n**__message__**:\n${message}`,
+                "embeds": null,
+                "username": "IEEE RAS PESU SITE",
+                "avatar_url": "https://www.ieee-ras.org/images/files/template/ieee-ras-logo.png",
+                "attachments": []
+            }
+        )
+    })
     res.status(201).json({ msg: 'Success' });
-  } catch (error) {
-    console.log(error);
+  }catch(e){
     res.status(500).json({ msg: 'Failed' });
   }
-};
+}
+

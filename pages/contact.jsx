3e21a2ebab79eb3795613c0200d1cd5ee3b68d3e
Anshul Ranjan <ItsAnshulRanjan@gmail.com>
2023-06-16 13:53:39 +0530

    new changes

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -10,7 +10,7 @@ const ContactPage = () => {
 
   const submitForm = async (e) => {
     e.preventDefault();
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/contact`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/contact.js`, {
       method: 'POST',
       body: JSON.stringify({ name, email, subject, message }),
     });
